7e70915748e9e6c95db36bde3b3ac0ff35eb088f
Sébastien Villemot <sebastien@dynare.org>
2025-07-03 17:58:48 +0200

    Build with GCC on all platforms (i.e. incl. macOS and FreeBSD)

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -25,7 +25,9 @@ if [[ "${target}" == *-apple-* ]]; then
     popd
 fi
 
-meson setup --cross-file="${MESON_TARGET_TOOLCHAIN}" --cross-file=flex.ini --buildtype=release build
+# We enforce GCC on all platforms (i.e. incl. macOS and FreeBSD), by using the GCC toolchain file
+# See https://docs.binarybuilder.org/stable/build_tips/#Using-GCC-on-macOS-and-FreeBSD
+meson setup --cross-file="${MESON_TARGET_TOOLCHAIN%.*}_gcc.meson" --cross-file=flex.ini --buildtype=release build
 
 ninja -j${nproc} -v -C build
 ninja -C build install
